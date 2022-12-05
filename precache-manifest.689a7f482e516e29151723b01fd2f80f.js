ea566f614e9ede60e70138f621e961e237d35305
abhijeet <abhijeetmane536@gmail.com>
2022-12-05 17:25:17 +0530

    Updates

---
+++ precache-manifest.689a7f482e516e29151723b01fd2f80f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e25123fe41a67060958",
+    "url": "/my-website/static/css/main.7a993776.chunk.css"
+  },
+  {
+    "revision": "1e25123fe41a67060958",
+    "url": "/my-website/static/js/main.1e25123f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/my-website/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "9eb36da8f011ac1a39d4",
+    "url": "/my-website/static/js/runtime~main.9eb36da8.js"
+  },
+  {
+    "revision": "0b57df78f79621302dba58ba300398f8",
+    "url": "/my-website/index.html"
+  }
+];
