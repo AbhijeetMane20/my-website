55c0a83d1e7998a9819c14b2c9c750b5d92b8979
abhijeet <abhijeetmane536@gmail.com>
2022-12-05 17:13:11 +0530

    hosting on github page

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+  imagebaseurl: "https://github.com/AbhijeetMane20",
+  name: "Abhijeet Mane",
+  role: "Frontend Developer(React.js)",
+  linkedinId: "https://www.linkedin.com/in/abhijeet-mane/",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, i have 1+ experience of react web development in two live projects.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/abhijeet-mane/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/AbhijeetMane20",
+      className: "fa fa-github",
+    },
+    {
+      name: "facebook",
+      url: "https://www.facebook.com/abhi.mane.143/",
+      className: "fa fa-facebook",
+    },
+  ],
+  aboutme:
+    "I am currently looking for an job opportunity.I am having 1 yr internship experience in web development. I am a self taught Full Stack Web Developer, currently diving deeper full stack web development. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "India",
+  website: "https://abhijeetmane20.github.io/my-website",
+  education: [
+    {
+      UniversityName: "Sant Gadge Baba University Amaravati",
+      specialization: "",
+      MonthOfPassing: "May",
+      YearOfPassing: "2022",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Evalbee",
+      specialization: "Software Developer",
+      YearOfLeaving: "2022",
+      Achievements:
+        "Learn basics of web development, working on techmologies like reactjs, speingboot, working on 2 live projects",
+    },
+    {
+      CompanyName: "EmbedGallery",
+      specialization: "Embedded Developer",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2021",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "SpringBoot",
+    },
+    {
+      skillname: "Git/Firebase Athentication",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Evalbee",
+      description: "Webapp",
+      imgurl: "images/portfolio/evalbee.png",
+      link:"https://evalbee.com/",
+    },
+    {
+      name: "PragatiDashboard",
+      description: "Webapp",
+      imgurl: "images/portfolio/pragati.png",
+      link:"http://dashboard.pragati.dignityeducation.org/",
+    },
+    {
+      name: "EmbedGallery",
+      description: "embedded project",
+      imgurl: "images/portfolio/samarth.jpeg",
+      link:"https://www.embedgallery.com/"
+    },
+  ],
+  testimonials: [
+    {
+      description: "Abhijeet has worked under me as intern, he has potential to become succssesful full stack developer",
+      name: "Bhimsen Padalkar(Sr.Software Developer)",
+    },
+  ],
+};
+
+export default resumeData;
